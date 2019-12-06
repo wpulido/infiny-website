1c249146517029b88cfd8fb4cda4b4575f40d0e9
Wistre Pulido <jstr.pulido9@gmail.com>
2019-12-06 17:20:39 -0400

    navegación funcional e imágenes añadidas

---
+++ app/js/infiny.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("open").style.display = "none";
+  document.getElementById("navEffects").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("close").style.display = "visible";
+  document.getElementById("navEffects").style.width = "0%";
+}
