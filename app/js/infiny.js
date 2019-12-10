5fb980bb8c593cf80afa254946cb9d122a5b2032
Wistre Pulido <jstr.pulido9@gmail.com>
2019-12-10 14:09:59 -0400

    home casi listo

--- app/js/infiny.js
+++ app/js/infiny.js
@@ -1,3 +1,5 @@
+// Navigation function
+
 function openNav() {
   document.getElementById("open").style.display = "none";
   document.getElementById("navEffects").style.width = "100%";
@@ -7,3 +9,7 @@ function closeNav() {
   document.getElementById("close").style.display = "visible";
   document.getElementById("navEffects").style.width = "0%";
 }
+
+// Year function
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
